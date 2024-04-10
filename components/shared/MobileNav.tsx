0071affb67870604b4144611f21da895a5901873
almustanir <olajidemubarak561@gmail.com>
2024-04-10 22:26:37 +0100

    mub4

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -33,7 +33,7 @@ const MobileNav = () => {
                 />
                 <Separator className="border border-gray-50" />
                 <NavItems/>
-                     <h4>ljkbjvhaddvikcvbxnbhxvchvu ccndvhxnchcvn cbhdbsvvx</h4>
+                    
                 </SheetContent>
             </Sheet>
 
